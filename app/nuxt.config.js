87ab2509c9b4c95cfe81a933d5d2d32de859ace5
Semion Shulman <shulman.dev@gmail.com>
2020-09-05 17:30:58 +0300

    PROD env variable

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -69,7 +69,7 @@ export default {
     defaultAssets: false
   },
   axios: {
-    baseURL: process.env.API_BASEURL+":"+process.env.API_PORT
+    baseURL: process.env.API_BASEURL+(process.env.PROD ? "" : ":"+process.env.API_PORT)
   },
   auth: {
     strategies: {
