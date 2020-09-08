ca88ebf612d8fc2faa9040da1dbe5a041f300367
Semion Shulman <shulman.dev@gmail.com>
2020-09-08 15:53:01 +0300

    Code cleanup, icons size fixed

--- app/layouts/app.vue
+++ app/layouts/app.vue
@@ -1,6 +1,15 @@
 <template>
   <v-app :style="{ backgroundColor: '#dbd4c4' }">
+    <v-app-bar class="rtl" flat height="30px" color="#dbd4c4" :style="{borderBottom: '1px solid #fff'}">
+      <v-toolbar-title class="subtitle-2" :style="{color:'#333'}">ברוך הבא למסעדת Drunk Babushka</v-toolbar-title>
+    </v-app-bar>
     <Nuxt />
+    <br/>
+    <v-footer app color="#dbd4c4" class="white--text" :style="{borderTop: '1px solid #fff'}">
+      <span>servicepls.me</span>
+      <v-spacer/>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -11,5 +20,7 @@ export default {
 </script>
 
 <style scoped>
-
+.rtl{
+  direction: rtl;
+}
 </style>
