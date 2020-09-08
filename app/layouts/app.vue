8a2853a7d94c955033987d155608ab5fe75c1939
Semion Shulman <shulman.dev@gmail.com>
2020-09-08 16:18:27 +0300

    Remove app bar

--- app/layouts/app.vue
+++ app/layouts/app.vue
@@ -1,11 +1,8 @@
 <template>
   <v-app :style="{ backgroundColor: '#dbd4c4' }">
-    <v-app-bar class="rtl" flat height="30px" color="#dbd4c4" :style="{borderBottom: '1px solid #fff'}">
-      <v-toolbar-title class="subtitle-2" :style="{color:'#333'}">ברוך הבא למסעדת Drunk Babushka</v-toolbar-title>
-    </v-app-bar>
     <Nuxt />
     <br/>
-    <v-footer color="#dbd4c4" class="white--text" :style="{borderTop: '1px solid #fff'}">
+    <v-footer app color="#dbd4c4" class="white--text" :style="{borderTop: '1px solid #fff'}">
       <span>servicepls.me</span>
       <v-spacer/>
       <span>&copy; {{ new Date().getFullYear() }}</span>
